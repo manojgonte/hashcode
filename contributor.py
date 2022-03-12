963316d50c3585f7a36abc624347f89ee468c5bc
Ketan Wangade <wangadeketan@gmail.com>
2022-03-12 23:46:24 +0530

    Added contributor file

---
+++ contributor.py
@@ -0,0 +1,15 @@
+'''
+Created by : Ketan Wangade
+Date : 12 March 2022
+Purpose : Hash code compition file
+'''
+
+class Contributor:
+    """
+    Here we are managing the contributor data
+    """
+    def __init__(self, name, skill):
+        self.name = name
+        self.skill = skill
+    
+
